18810b8b718bb62d0b85a2a528577c50ca30eda0
averageincome <61590237+averageincome@users.noreply.github.com>
2022-10-20 09:53:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import 'https://github.com/averageincome/diginn/blob/main/style.css';
+import url('https://github.com/averageincome/diginn/blob/main/style.css');
 import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
